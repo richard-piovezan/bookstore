2cab2491ba4a1a6fc2c3d9f44e7d97dfc594cd58
richard-piovezan <contato.piovezan@gmail.com>
2025-08-31 18:03:22 -0300

    feat: create tests to auth token

--- order/tests/test_viewsets/test_order_viewset.py
+++ order/tests/test_viewsets/test_order_viewset.py
@@ -3,6 +3,7 @@ import json
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient, APITestCase
+from rest_framework.authtoken.models import Token
 
 from order.factories import OrderFactory, UserFactory
 from order.models import Order
@@ -15,6 +16,13 @@ class TestOrderViewSet(APITestCase):
     client = APIClient()
 
     def setUp(self):
+        self.user = UserFactory()
+        token = Token.objects.create(user=self.user)
+        token.save()
+
+        token = Token.objects.get(user__username=self.user.username)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+
         self.category = CategoryFactory(title="technology")
         self.product = ProductFactory(
             title="mouse", price=100, category=[self.category]
@@ -28,19 +36,10 @@ class TestOrderViewSet(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         order_data = json.loads(response.content)
-        self.assertEqual(
-            order_data[0]["product"][0]["title"], self.product.title
-        )
-        self.assertEqual(
-            order_data[0]["product"][0]["price"], self.product.price
-        )
-        self.assertEqual(
-            order_data[0]["product"][0]["active"], self.product.active
-        )
-        self.assertEqual(
-            order_data[0]["product"][0]["category"][0]["title"],
-            self.category.title,
-        )
+        self.assertEqual(order_data['results'][0]['product'][0]['title'], self.product.title)
+        self.assertEqual(order_data['results'][0]['product'][0]['price'], self.product.price)
+        self.assertEqual(order_data['results'][0]['product'][0]['active'], self.product.active)
+        self.assertEqual(order_data['results'][0]['product'][0]['category'][0]['title'], self.category.title)
 
     def test_create_order(self):
         user = UserFactory()
